95968b6f7438bc8c176d7aca9cd1189733fbf1e2
chandra-98a <chandrakalaa2022@gmail.com>
2023-02-03 13:45:40 +0530

    "java type conversion examples"

---
+++ src/DoubleToString/DoubleToStringExample2.java
@@ -0,0 +1,9 @@
+package DoubleToString;
+
+
+public class DoubleToStringExample2{
+    public static void main(String args[]){
+        double d=89.7;
+        String s=Double.toString(d);
+        System.out.println(s);
+    }}
---
+++ src/StringToObject/StringToObjectExample.java
@@ -0,0 +1,9 @@
+package StringToObject;
+
+
+public class StringToObjectExample{
+    public static void main(String args[]){
+        String s="hello";
+        Object obj=s;
+        System.out.println(obj);
+    }}
